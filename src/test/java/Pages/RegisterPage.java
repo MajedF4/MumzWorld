fa6a4ad01e6461e15d7a4c1301469730a24200f3
Abdelmagid Mohamed <abdelmagid.mohamed@nctr.sd>
2023-12-01 00:50:28 +0200

    create registration fun tow and add it's locators

--- src/test/java/Pages/RegisterPage.java
+++ src/test/java/Pages/RegisterPage.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import Utilires.Reusable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
@@ -10,6 +11,7 @@ import java.time.Duration;
 public class RegisterPage {
 
     WebDriver driver;
+    Reusable reusable = new Reusable();
     By registerBtn = By.xpath("(//span[text()='Register'])[1]");
     By firstName = By.id("create_account_page_firstname");
     By lastName = By.id("create_account_page_lastname");
@@ -20,9 +22,10 @@ public class RegisterPage {
     By city = By.id("customer_city");
     By street = By.id("customer_street[0]");
     By building = By.id("customer_street[1]");
-    By phoneNumber = By.className("customer_telephone");
+    By phoneNumber = By.id("customer_telephone");
     By selectCountry = By.xpath("(//div[contains(@class,'searchSelect-input-D63 observableSelect-input-2nD')])[1]");
     By saveAndContinue = By.className("addressForm-buttons-1-h");
+    By selectedOp = By.xpath("//div[text()='United Kingdom']");
     public RegisterPage(WebDriver driver) {
         this.driver = driver;
     }
@@ -38,4 +41,16 @@ public class RegisterPage {
         driver.findElement(registerBtn).click();
         wait.until(d -> driver.findElement(stepTwoLanding).isDisplayed());
     }
+
+    public void registrationStepTwo(String userCity, String userStreet, String userBuilding, String userPhone){
+        reusable.selectFun(driver,selectCountry, selectedOp);
+        driver.findElement(city).sendKeys(userCity);
+        driver.findElement(street).sendKeys(userStreet);
+        driver.findElement(building).sendKeys(userBuilding);
+        driver.findElement(phoneNumber).sendKeys(userPhone);
+        if (driver.findElement(saveAndContinue).isEnabled()) {
+            driver.findElement(saveAndContinue).click();
+        }
+    }
+
 }
