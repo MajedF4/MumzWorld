f373d69c11bc43b1394ea352b62fd09e1dc5a8da
Abdelmagid Mohamed <abdelmagid.mohamed@nctr.sd>
2023-11-30 23:09:06 +0200

    create Registerpage and it's constrictor

--- src/test/java/Pages/RegisterPage.java
+++ src/test/java/Pages/RegisterPage.java
@@ -1,4 +1,16 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class RegisterPage {
+
+    WebDriver driver;
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void registrationStepOne(){
+
+    }
 }
