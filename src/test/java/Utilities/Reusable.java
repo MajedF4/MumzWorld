251216294c9de716a258b0ad1e744dbf2eb1e31b
Abdelmagid Mohamed <abdelmagid.mohamed@nctr.sd>
2023-12-12 09:02:22 +0200

    create wait event fun for displayed elements

--- src/test/java/Utilities/Reusable.java
+++ src/test/java/Utilities/Reusable.java
@@ -2,10 +2,18 @@ package Utilities;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Reusable {
     public void selectFun(WebDriver driver, By selector, By index){
         driver.findElement(selector).click();
         driver.findElement(index).click();
+    }
+    public void isDisplayedWaitEvent(WebDriver wDriver, By elementLocator, int duration) {
+        Wait wait = new WebDriverWait(wDriver, Duration.ofSeconds(duration));
+        wait.until(d -> wDriver.findElement(elementLocator).isDisplayed());
     }
 }
